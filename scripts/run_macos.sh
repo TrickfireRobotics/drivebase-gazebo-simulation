232ecbaeb997d4a7de8e69d2f9bd23db832fdce1
Matej Stastny <matysta@outlook.com>
2025-11-13 19:51:24 -0800

    Fixed entrypoint not being run when connecting to the container by rerunning it in the run script

--- scripts/run_macos.sh
+++ scripts/run_macos.sh
@@ -14,11 +14,11 @@ sleep 2
 
 # Allow local connections
 xhost + 127.0.0.1 >/dev/null 2>&1 || true
-xhost + localhost  >/dev/null 2>&1 || true
+xhost + localhost >/dev/null 2>&1 || true
 
 # Docker Desktop forwards host.docker.internal to the host
 export DISPLAY=host.docker.internal:0
 
 docker compose up -d
 echo "Entering rover-sim container..."
-docker exec -it rover-sim bash
+docker exec -it rover-sim /entrypoint.sh bash -i
